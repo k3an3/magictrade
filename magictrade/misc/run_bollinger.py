c57989e95fb6acec45b086b13c510574d2f965e7
Keane O'Kelley <keane.m.okelley@gmail.com>
2020-07-22 15:22:41 -0400

    Require queue name
    Signed-off-by: Keane O'Kelley <keane.m.okelley@gmail.com>

--- magictrade/misc/run_bollinger.py
+++ magictrade/misc/run_bollinger.py
@@ -55,7 +55,7 @@ def main(args):
 def cli():
     parser = ArgumentParser(description="Place Bollinger Bend trades.",
                             formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-q', '--trade-queue', help="Name of the magictrade queue to add trades to.")
+    parser.add_argument('-q', '--trade-queue', required=True, help="Name of the magictrade queue to add trades to.")
     parser.add_argument('-d', '--dry-run', action="store_true", help="Set the dry run flag to tell the backend to "
                                                                      "check if trades are "
                                                                      "suitable, but shouldn't be placed.")
