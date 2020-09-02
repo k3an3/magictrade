7a3618197937a1c16e8ee2791187e58890404b64
Keane O'Kelley <keane.m.okelley@gmail.com>
2020-09-01 21:31:41 -0400

    trade_count vs ticker count

--- magictrade/misc/run_bollinger.py
+++ magictrade/misc/run_bollinger.py
@@ -10,18 +10,23 @@ from magictrade.strategy.bollinger import BollingerBendStrategy, INDEX
 from magictrade.trade_queue import RedisTradeQueue
 from magictrade.utils import get_all_trades, bool_as_str
 
-TICKERS = (
-    "AAPL", "ABBV", "ADBE", "AMAT", "AMD", "AMGN", "AMZN", "ATVI", "AVGO", "AXP", "AZO", "BA", "BABA", "BAC", "BIDU",
-    "BKNG", "BMY", "BP", "BYND", "C", "CAT", "CGC", "CMCSA", "CMG", "COST", "CRM", "CSCO", "CVS", "CVX", "DAL", "DE",
-    "DIS", "DOW", "DRI", "EXPE", "FB", "FDX", "FIVE", "GILD", "GIS", "GOOGL", "GS", "HD", "HON", "IBM", "INTC", "ISRG",
-    "JNJ", "JPM", "KMX", "KO", "LMT", "LOW", "LRCX", "LULU", "LVS", "LYFT", "MA", "MCD", "MELI", "MMM", "MRK", "MS",
-    "MSFT", "MU", "NFLX", "NKE", "NOC", "NVDA", "NXPI", "PEP", "PFE", "PG", "PXD", "PYPL", "QCOM", "RH", "ROKU", "RTX",
-    "SBUX", "SHOP", "SQ", "SWKS", "T", "TGT", "TRV", "TSLA", "TSN", "TTD", "TWLO", "TWTR", "TXN", "UBER", "ULTA", "UNH",
-    "UNP", "UPS", "URI", "V", "VZ", "WBA", "WDAY", "WDC", "WMT", "WYNN", "XOM", "YUM")
+TICKERS = ("AAPL", "ABBV", "ADBE", "AMAT", "AMD", "AMGN", "AMZN", "ATVI",
+           "AVGO", "AXP", "AZO", "BA", "BABA", "BAC", "BIDU", "BKNG", "BMY",
+           "BP", "BYND", "C", "CAT", "CGC", "CMCSA", "CMG", "COST", "CRM",
+           "CSCO", "CVS", "CVX", "DAL", "DE", "DIS", "DOW", "DRI", "EXPE",
+           "FB", "FDX", "FIVE", "GILD", "GIS", "GOOGL", "GS", "HD", "HON",
+           "IBM", "INTC", "ISRG", "JNJ", "JPM", "KMX", "KO", "LMT", "LOW",
+           "LRCX", "LULU", "LVS", "LYFT", "MA", "MCD", "MELI", "MMM", "MRK",
+           "MS", "MSFT", "MU", "NFLX", "NKE", "NOC", "NVDA", "NXPI", "PEP",
+           "PFE", "PG", "PXD", "PYPL", "QCOM", "RH", "ROKU", "RTX", "SBUX",
+           "SHOP", "SQ", "SWKS", "T", "TGT", "TRV", "TSLA", "TSN", "TTD",
+           "TWLO", "TWTR", "TXN", "UBER", "ULTA", "UNH", "UNP", "UPS", "URI",
+           "V", "VZ", "WBA", "WDAY", "WDC", "WMT", "WYNN", "XOM", "YUM")
 
 
 def main(args):
-    print("Starting Bollinger Bend runner at", datetime.datetime.now().isoformat())
+    print("Starting Bollinger Bend runner at",
+          datetime.datetime.now().isoformat())
     if args.random_sleep:
         seconds = random.randint(*args.random_sleep)
         print(f"Sleeping for {seconds}s.")
@@ -31,7 +36,8 @@ def main(args):
     positions = set()
     try:
         if args.account_id:
-            positions = set([t['data']['symbol'] for t in get_all_trades(args.account_id)])
+            positions = set(
+                [t['data']['symbol'] for t in get_all_trades(args.account_id)])
     except AttributeError:
         pass
 
@@ -45,8 +51,17 @@ def main(args):
         exit(0)
 
     now = datetime.datetime.now()
-    close = datetime.datetime(year=now.year, month=now.month, day=now.day, hour=16, minute=0, second=0, microsecond=0)
-    for ticker in random.sample(TICKERS, k=args.trade_count or len(TICKERS)):
+    close = datetime.datetime(year=now.year,
+                              month=now.month,
+                              day=now.day,
+                              hour=16,
+                              minute=0,
+                              second=0,
+                              microsecond=0)
+    trade_count = 0
+    for ticker in random.sample(TICKERS, k=args.ticker_count or len(TICKERS)):
+        if trade_count >= args.trade_count:
+            break
         if ticker in positions:
             print(f"{ticker} already in positions; skipping...")
             continue
@@ -59,7 +74,8 @@ def main(args):
         if not historic_closes:
             print(f"No ticker history for {ticker}; skipping...")
             continue
-        signal_1, signal_2, signal_3 = BollingerBendStrategy.check_signals(historic_closes)
+        signal_1, signal_2, signal_3 = BollingerBendStrategy.check_signals(
+            historic_closes)
         print(f"{ticker: <5}: {signal_1=}, {signal_2=}, {signal_3=}")
 
         if not args.dry_run and (signal_1 or signal_2 or signal_3):
@@ -73,23 +89,51 @@ def main(args):
                 "signal_2": bool_as_str(signal_2),
                 "signal_3": bool_as_str(signal_3),
             })
+            trade_count += 1
         # API has 60 calls/minute limit
         sleep(1)
+    print(f"{trade_count} trades placed.")
 
 
 def cli():
     parser = ArgumentParser(description="Place Bollinger Bend trades.",
                             formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-q', '--trade-queue', help="Name of the magictrade queue to add trades to.")
-    parser.add_argument('-d', '--dry-run', action="store_true", help="Set the dry run flag to check for trade "
-                                                                     "signals, but not actually place trades.")
-    parser.add_argument('-c', '--trade-count', default=0, type=int,
+    parser.add_argument('-q',
+                        '--trade-queue',
+                        help="Name of the magictrade queue to add trades to.")
+    parser.add_argument('-d',
+                        '--dry-run',
+                        action="store_true",
+                        help="Set the dry run flag to check for trade "
+                        "signals, but not actually place trades.")
+    parser.add_argument('-c',
+                        '--trade-count',
+                        default=0,
+                        type=int,
                         help="Max number of trades to place. 0 for unlimited.")
-    parser.add_argument('-l', '--allocation', type=int, default=1, help="Allocation percentage for each trade")
-    parser.add_argument('-r', '--random-sleep', type=int, nargs=2, metavar=('min', 'max'),
-                        help="Range of seconds to randomly sleep before running.")
-    parser.add_argument('-a', '--account-id', help='If set, will check existing trades to avoid securities '
-                                                   'with active trades.')
+    parser.add_argument(
+        '-n',
+        '--ticker-count',
+        default=0,
+        type=int,
+        help="Max number of tickers to consider. 0 for unlimited.")
+    parser.add_argument('-l',
+                        '--allocation',
+                        type=int,
+                        default=1,
+                        help="Allocation percentage for each trade")
+    parser.add_argument(
+        '-r',
+        '--random-sleep',
+        type=int,
+        nargs=2,
+        metavar=('min', 'max'),
+        help="Range of seconds to randomly sleep before running.")
+    parser.add_argument(
+        '-a',
+        '--account-id',
+        help='If set, will check existing trades to avoid securities '
+        'with active trades.')
     args = parser.parse_args()
     if not (args.dry_run or args.trade_queue):
         print("Error: Either --trade-queue or --dry-run are required.")
